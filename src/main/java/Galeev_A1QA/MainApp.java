6a9d0c7d6ad756f50e584db561f3e88762f0b80d
kanntaim <galileo174rus@gmail.com>
2018-11-02 18:36:55 +0300

    deleted comments and import

--- src/main/java/Galeev_A1QA/MainApp.java
+++ src/main/java/Galeev_A1QA/MainApp.java
@@ -1,15 +1,8 @@
 package Galeev_A1QA;
 
-import org.apache.camel.main.Main;
 
-/**
- * A Camel Application
- */
 public class MainApp {
 
-    /**
-     * A main() so we can easily run these routing rules in our IDE
-     */
     public static void main(String... args) throws Exception {
         System.out.print("Hello World!");
     }
