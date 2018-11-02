4e090319edb994576686224260e60cc945eb0ac9
kanntaim <galileo174rus@gmail.com>
2018-11-02 18:38:35 +0300

    changed print to println

--- src/main/java/Galeev_A1QA/MainApp.java
+++ src/main/java/Galeev_A1QA/MainApp.java
@@ -4,7 +4,7 @@ package Galeev_A1QA;
 public class MainApp {
 
     public static void main(String... args){
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
     }
 
 }
